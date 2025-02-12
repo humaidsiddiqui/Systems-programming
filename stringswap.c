669cdbcab8acb7454192277e84c43386d1c55ed5
Humaid Siddiqui <Humaidsiddiqui81@gmail.com>
2025-02-12 02:28:58 -0800

    swap function

--- stringswap.c
+++ stringswap.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef struct
 {
     char firstName[20];
     char lastName[20];
 } Person;
+
+void swap(void* first, void* second, size_t numBytes)
+{
+    void* temp = malloc(numBytes);
+    if (temp == NULL)
+    {
+        printf("Failed to allocate memory.\n");
+        exit(1);
+    }
+
+    memcpy(temp, first, numBytes);
+    memcpy(first, second, numBytes);
+    memcpy(second, temp, numBytes);
+
+    free(temp); 
+
+}
