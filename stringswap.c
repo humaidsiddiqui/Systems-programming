428e65e7dac3a8fb8a7205b8923156f5c875b6f8
Humaid Siddiqui <Humaidsiddiqui81@gmail.com>
2025-02-12 02:30:46 -0800

    arguements from the user

--- stringswap.c
+++ stringswap.c
@@ -24,3 +24,17 @@ void swap(void* first, void* second, size_t numBytes)
     free(temp); 
 
 }
+
+int main(int argc, char* argv[])
+{
+    int x, y;
+    Person first = {"Adam", "Smith"};
+    Person second = {"John", "Doe"};
+
+    if (argc != 3 ||
+        strcmp(argv[1], "-h") == 0 ||
+        strcmp(argv[1], "--help") == 0)
+    {
+        printf("Usage: %s n1 n2\n", argv[0]);
+        exit(1);
+    }
