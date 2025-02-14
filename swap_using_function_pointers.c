8736f40a6cbda7d78b6f310b98bbf3e7260d2109
Humaid Siddiqui <Humaidsiddiqui81@gmail.com>
2025-02-13 20:13:11 -0800

    modified function

---
+++ swap_using_function_pointers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/*
+ * Compile using:
+   cc stringmodify.c -o stringmodify
+ * Run using:
+   ./stringmodify message
+*/
+
+#define MESSAGE_LENGTH 200
+void modify(char* message, int (*visitor)(int))
+{
+    int index; 
+
+    if (message == NULL)
+    {
+        printf("message cannot be null.\n");
+        exit(1);
+    }
