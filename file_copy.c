a2b4883be158f1ce50e8d8d45ae75301e1d8d722
Humaid Siddiqui <humaidsiddiqui81@gmail.com>
2025-05-05 22:38:25 -0700

    open destination file for writing

--- file_copy.c
+++ file_copy.c
@@ -11,3 +11,57 @@
  Run using:
  ./copy source destination
 */
+
+#define BUFFER_SIZE 10
+int main(int argc, char* argv[])
+{
+    int sourceFd, destinationFd;
+    int status;
+    char buffer[BUFFER_SIZE]; // byte array
+    int numRead, numWritten;
+
+    if (argc != 3 ||
+        strcmp(argv[1], "-h") == 0 ||
+        strcmp(argv[1], "--help") == 0)
+    {
+        printf("Usage: %s source destination\n", argv[0]);
+        exit(1);
+    }
+
+    /* Open the source file for reading */
+    sourceFd = open(argv[1], O_RDONLY);
+    if (sourceFd == -1)
+    {
+        printf("Failed to open source file for reading.\n");
+        exit(1);
+    }
+     /* Open the destination file for writing */
+     destinationFd = open(
+        argv[2], 
+        O_CREAT | O_WRONLY | O_TRUNC,
+        S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+    );
+    if (destinationFd == -1)
+    {
+        printf("Failed to open destination file.\n");
+        exit(1);
+    }
+
+    numRead = read(sourceFd, buffer, BUFFER_SIZE);
+    while(numRead > 0)
+    {
+        numWritten = write(destinationFd, buffer, numRead);
+        if (numWritten < numRead)
+        {
+            printf("Partial write error.\n");
+            exit(1);
+        }
+        numRead = read(sourceFd, buffer, BUFFER_SIZE);
+    }
+
+    if (numRead == -1)
+    {
+        printf("Failed to read source file.\n");
+        exit(1);
+    }
+    
