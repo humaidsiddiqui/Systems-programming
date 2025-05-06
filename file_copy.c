250d9c8a43d5f111dae8685c8807d28262596627
Humaid Siddiqui <humaidsiddiqui81@gmail.com>
2025-05-05 22:36:36 -0700

    init commit

---
+++ file_copy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*
+ Compile using:
+ cc copy.c -o copy
+ Run using:
+ ./copy source destination
+*/
