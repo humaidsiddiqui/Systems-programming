8df27d3b19a4d46dbda48f03d6851e3a7756a9de
Humaid Siddiqui <humaidsiddiqui81@gmail.com>
2025-05-10 15:14:56 -0700

    header files

--- process_creation_2.c
+++ process_creation_2.c
@@ -4,3 +4,14 @@
  * Each child processes runs "date --utc", and then exits.
  * Parent process waits for the child processes to complete and then exits.
  */
+
+ #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void printBye()
+{
+    printf("Process %d says Bye!\n", getpid());
+}
