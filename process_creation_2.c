30573f6851529959788b27c2794493841d4bed42
Humaid Siddiqui <humaidsiddiqui81@gmail.com>
2025-05-10 15:16:42 -0700

    child process

--- process_creation_2.c
+++ process_creation_2.c
@@ -15,3 +15,64 @@ void printBye()
 {
     printf("Process %d says Bye!\n", getpid());
 }
+int main()
+{
+    pid_t parentPid, child;
+    pid_t childPid[2];
+    int childStatus, status, index;
+
+    /* Register the exit handler */
+    status = atexit(printBye);
+    if (status != 0)
+    {
+        printf("Failed to register exit handler.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (index = 0; index < 2; index++)
+    {
+        childPid[index] = fork();
+
+        switch(childPid[index])
+        {
+            case -1: /* Child creation failed */
+                printf("Failed to create child process.\n");
+                exit(EXIT_FAILURE);
+            case 0: /* Child process */
+                printf(
+                    "I am the child process with PID %d. My parent PID is %d.\n",
+                    getpid(),
+                    getppid()
+                );
+
+                status = execlp("/usr/bin/date", "date", "--utc", NULL);
+                /* Control in the child process should not reach the next line unless exec fails */
+                printf("Failed to exec.\n");
+                _exit(EXIT_FAILURE);
+
+            default: /* Parent process */
+                break;
+        }
+ 
+    }
+
+    for (index = 0; index < 2; index++)
+    {
+        child = waitpid(childPid[index], &childStatus, 0);
+        if (child == -1)
+        {
+            printf("Failed to wait for child process.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        printf(
+            "Child process with PID %d exited with status %d.\n",
+            child,
+            childStatus
+        );
+    }
+
+    exit(EXIT_SUCCESS);
+}
+    
+
